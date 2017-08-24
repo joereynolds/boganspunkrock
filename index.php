84d265ff811a4c4df9222c0a8946a474035c1e6a
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2017-08-24 13:44:53 +0100

    Add simplegrid

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <?php include './sections/head.htm' ?>
+  </head>
+
+    <body>
+      <?php include './sections/header.htm' ?>
+      <main class="grid grid-pad">
+        <?php include './pages/contact.htm' ?>
+        <?php include './pages/home.htm' ?>
+      </main>
+    </body>
+</html>
