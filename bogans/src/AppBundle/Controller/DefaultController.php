3c2dfba785e6f67591bfa00bc28e62add11c1daf
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2017-08-25 14:01:54 +0100

    refactor views, add news page

--- bogans/src/AppBundle/Controller/DefaultController.php
+++ bogans/src/AppBundle/Controller/DefaultController.php
@@ -11,11 +11,34 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'page' => 'home'
         ]);
     }
+
+    /**
+     * @Route("/contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('default/index.html.twig', [
+            'page' => 'contact'
+        ]);
+    }
+
+    /**
+     * @Route("/news")
+     */
+    public function newsAction()
+    {
+        return $this->render('default/index.html.twig', [
+            'page' => 'news'
+        ]);
+    }
+
+
+
+
 }
