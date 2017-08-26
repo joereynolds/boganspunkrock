6763cc92d023dc0356f0c6e84384d53ecaa464ad
Joe Reynolds <joereynolds952@gmail.com>
2017-08-26 23:41:10 +0100

    send email. Fix header padding

--- bogans/src/AppBundle/Controller/DefaultController.php
+++ bogans/src/AppBundle/Controller/DefaultController.php
@@ -23,6 +23,16 @@ class DefaultController extends Controller
      */
     public function contactAction()
     {
+        $request = Request::createFromGlobals();
+
+        if ($request->request->get('email')) {
+            mail(
+                'boganspunkrock@gmail.com',
+                "You've got mail from the site!",
+                $request->request->get('text')
+            );
+        }
+
         return $this->render('default/index.html.twig', [
             'page' => 'contact'
         ]);
