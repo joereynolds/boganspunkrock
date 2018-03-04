cf5d6ca555802b4b3f1b1b031258978b1d9e903e
Joe Reynolds <joereynolds952@gmail.com>
2018-03-04 11:07:16 +0000

    use env file for mysql

--- bogans/after.sh
+++ bogans/after.sh
@@ -3,6 +3,9 @@
 # know when a container is really 'up'
 # Once you think it's up, run this.
 
+# Composer install
+docker run --rm --interactive --tty --volume $PWD:/app composer install
+
 # Change the permissions because Symfony 3 is a PITA with them.
 docker exec bogans_php_1 chown -R www-data:www-data var
 
@@ -11,7 +14,3 @@ docker exec bogans_php_1 php bin/console doctrine:migrations:migrate
 
 # Whilst we're at it, populate the tables with some fake data
 docker exec bogans_php_1 php bin/console doctrine:fixtures:load
-
-
-
-
