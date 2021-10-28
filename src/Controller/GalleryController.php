b9a734806757a2fe0b865f0d2a6e589029d67a79
Joe Reynolds <joereynolds952@gmail.com>
2021-10-28 15:00:20 +0100

    Start on listing images in gallery

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -9,6 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 final class GalleryController extends AbstractController
 {
+    const BUCKET = 'bogans-storage-bucket';
+
+    const S3_BASE_URL = self::BUCKET . '.s3.eu-west-2.amazonaws.com/';
+
+    /**
+     * @var S3Client
+     */
+    private $client;
+
     public function __construct(S3Client $client)
     {
         $this->client = $client;
@@ -17,6 +26,24 @@ final class GalleryController extends AbstractController
     #[Route('/gallery', name: 'gallery')]
     public function index(): Response
     {
-        return $this->render('pages/gallery.html.twig', []);
+        $candidImages = $this->client->listObjectsV2([
+            'Bucket' => self::BUCKET,
+            'Prefix' => 'images/doc',
+            'MaxKeys' => 5
+        ]);
+
+        // TODO - create a Gallery object (name, and images)
+        $candidGallery = [
+            'name' => 'Our Rehearsal Space',
+            'images' => $candidImages->get('Contents')
+        ];
+
+        return $this->render(
+            'pages/gallery.html.twig',
+            [
+                's3Url' => self::S3_BASE_URL,
+                'galleries' => [$candidGallery]
+            ]
+        );
     }
 }
