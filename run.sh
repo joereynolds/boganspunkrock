d6f5bdc35ed3710db680007029d8108f6048b692
Joe Reynolds <joereynolds952@gmail.com>
2021-10-28 09:49:52 +0100

    Add composer install to run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ docker-compose down
 docker-compose rm -v
 
 docker-compose up --force-recreate --build
-
+docker run --rm --interactive --tty --volume $PWD:/app composer install
 
 # change this to a docker healthcheck
 echo 'Once containers are up, run ./after.sh'
