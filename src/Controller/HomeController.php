1c8ebedf336b97307ea0bea505fac878cffc438b
Joe Reynolds <joereynolds952@gmail.com>
2021-11-12 21:16:13 +0000

    Fallback to [] if TableNotFoundException is thrown
    This will stop the homepage from falling on its arse.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Exception\TableNotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,16 +21,25 @@ class HomeController extends AbstractController
     public function __construct(EntityManagerInterface $em)
     {
         $this->gigRepository = $em->getRepository('App\Entity\Gig');
+
     }
 
     #[Route('/')]
     public function index(): Response
     {
+        try {
+            $upcomingGigs = $this->gigRepository->findUpcomingGigs();
+            $pastGigs = $this->gigRepository->findPastGigs(self::GIG_LIMIT);
+        } catch (TableNotFoundException $e) {
+            $upcomingGigs = [];
+            $pastGigs = [];
+        }
+
         return $this->render(
             'pages/home.html.twig',
             [
-                'gigs' => $this->gigRepository->findUpcomingGigs(),
-                'pastGigs' => $this->gigRepository->findPastGigs(self::GIG_LIMIT)
+                'gigs' => $upcomingGigs,
+                'pastGigs' => $pastGigs
             ]
         );
     }
