945a1c7ad894f8277521d94f576d061ab0a3e67e
Joe Reynolds <joereynolds952@gmail.com>
2022-01-29 10:39:43 +0000

    Fix composer not running in ./run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 docker-compose down
 docker-compose rm -v
 
+# Ignoring platform reqs here because the php version in the composer container is different
+# to the one inside our containers
+docker run --rm --interactive --tty --volume $PWD:/app composer install --ignore-platform-reqs
 docker-compose up --force-recreate --build
-docker run --rm --interactive --tty --volume $PWD:/app composer install
 
 # change this to a docker healthcheck
 echo 'Once containers are up, run ./after.sh'
